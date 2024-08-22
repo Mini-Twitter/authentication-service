c5a66f8b26f13c285f893b7e81bfb7553530169b
Jons <ozodjon160605@gmail.com>
2024-08-22 13:08:33 +0500

    ok

--- migrations/000001_auth_service_table.up.sql
+++ migrations/000001_auth_service_table.up.sql
@@ -1,9 +1,8 @@
 create table users(
     id uuid default gen_random_uuid() primary key,
-    first_name varchar,
-    last_name varchar,
-    email varchar unique,
     phone varchar(13)unique ,
+    email varchar unique,
+    password varchar,
     created_at timestamp default now(),
     updated_at timestamp default now(),
     deleted_at bigint default 0,
@@ -12,8 +11,9 @@ create table users(
 
 create table user_profile(
     user_id uuid references users(id),
+    first_name varchar,
+    last_name varchar,
     username varchar unique,
-    password varchar,
     nationality varchar,
     bio varchar,
     profile_image varchar,
