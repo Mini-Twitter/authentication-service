bcaa2c4e4760bcde092706b7035ed65fa9ca1b27
xamidullox <xamidulloxxamidullayev592>
2024-08-26 17:00:54 +0500

    mid

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"auth-service/pkg/models"
 	"auth-service/pkg/token"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -9,25 +8,20 @@ import (
 
 func GetAccessTokenMid() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// Cookie'dan tokenni olish
 		tokenString, err := ctx.Cookie("refresh_token")
 		if err != nil {
-			ctx.JSON(http.StatusUnauthorized, models.Error{"Authorization cookie not found"})
+			ctx.JSON(http.StatusUnauthorized, err)
 			ctx.Abort()
 			return
 		}
 
-		// JWT tokenni tekshirish va tasdiqlash
 		claims, err := token.ExtractClaimsRefresh(tokenString)
 		if err != nil {
-			ctx.JSON(http.StatusUnauthorized, models.Error{
-				Error: "Invalid token",
-			})
+			ctx.JSON(http.StatusUnauthorized, err)
 			ctx.Abort()
 			return
 		}
 
-		// Foydalanuvchi ma'lumotlarini context ga qo'shish
 		ctx.Set("claims", claims)
 
 		ctx.Next()
